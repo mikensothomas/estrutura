03015ba70e34b9c551ed2bb35026c046a7965e83
Mikenson Thomas <thomasmikenson40@gmail.com>
2023-12-30 08:16:04 -0300

    Commit

--- tabelaHash.c
+++ tabelaHash.c
@@ -4,10 +4,78 @@
 
 #define TAM 31
 
+void inicializar_tabela(int t[]){
+    for (int  i = 0; i < TAM; i++){
+        t[i] = 0;
+    }
+}
 
+int funcaoHash(int chave){
+    return chave % TAM;
+}
+
+void inserir_na_tabela(int t[], int valor){
+    int id = funcaoHash(valor);
+
+    if(t[id] == 0){
+        t[id] = valor;
+    }
+}
+
+int buscar_na_tabela(int t[], int chave){
+    int id = funcaoHash(chave);
+    printf("O indice gerado é: %d e a chave gerada é: %d \n", id, t[id]);
+    
+    while (t[id] == 0){
+
+        if(t[id] == chave){
+            return chave;
+        } else {
+            id = funcaoHash(id + 1);
+        }
+    }
+    return 0;
+}
+
+void imprimir_tabela(int t[]){
+    for (int i = 0; i < TAM; i++){
+        printf("%d = %d\n", i, t[i]);
+    }
+}
 
 int main(){
+    int opcao, numero, tabela[TAM];
+    int id = funcaoHash(numero);
+    inicializar_tabela(tabela);
 
+    do{
+      printf("\n\t0 - Sair\n\t1 - Inserir\n\t2 - Buscar\n\t3 - Imprimir\n");
+      scanf("%d", &opcao);
+      printf("\n");
 
+      switch (opcao){
+      case 0:
+        exit(0);
+        break;
+    case 1:
+        printf("Digite o número a ser inserido: ");
+        scanf("%d", &numero);
+        inserir_na_tabela(tabela, numero);
+        break;
+    case 2:
+        printf("Digite o número a ser buscado: ");
+        scanf("%d", &numero);
+        buscar_na_tabela(tabela, numero);
+        break;
+    case 3:
+        imprimir_tabela(tabela);
+        printf("\n");
+        break;
+      default:
+        printf("Valor inválido.\n");
+        break;
+      }
+    } while (opcao);
+    
     return 0;
 }
